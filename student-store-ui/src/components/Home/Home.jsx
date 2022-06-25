5731ec09bf17affa7fd12a12164cd6b3b0d1ebda
Nuha Qadir <nuhaqadir@fb.com>
2022-06-24 21:54:51 -0700

    done

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -10,6 +10,19 @@ export default function Home(props) {
       <ProductGrid shoppingCart={props.shoppingCart} products={props.products} 
       handleRemoveItemToCart={props.handleRemoveItemToCart}
       handleAddItemToCart={props.handleAddItemToCart} setFetching={props.setFetching}/>
+      <div className="about-us" id="about-us">
+          <h3>About</h3>
+          <div className="summary">
+            <div className="text">
+              <p>The codepath student store offers great products at great prices from a great team and for a great cause.</p>
+              <p>We've searched far and wide for items that perk the interests of even the most eccentric students and decided to offer them all here in one place.</p>
+              <p>All proceeds go towards bringing high quality CS education to college students around the country.</p>
+            </div>
+              {/* <div class="media">
+                <img src="/assets/giant_codepath.6952ef57.svg" alt="shop pic"/>
+              </div> */}
+          </div>
+      </div>
     </div>
   )
 }
